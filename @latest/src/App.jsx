f379c71e4417db07497c7abbb99d56d8cb285915
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-10-10 11:58:30 +0600

    footer added

--- @latest/src/App.jsx
+++ @latest/src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/Navbar/navbar";
 import Home from "./components/Home/home";
+import Footer from "./components/Footer/footer";
 
 function App() {
   return (
     <>
       <Navbar />
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ @latest/src/components/Footer/footer.jsx
@@ -0,0 +1,105 @@
+import "./footer.css";
+import { Container, Row, Col } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPhone,
+  faEnvelope,
+  faMapMarkerAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Footer = () => {
+  return (
+    <footer className="bg-light text-dark py-4">
+      <Container className="">
+        <Row className="text-center text-md-start">
+          {/* Quick Links */}
+          <Col md={4} className="mb-3">
+            <h5 className="fw-bold">Quick Links</h5>
+            <ul className="list-unstyled">
+              <li>
+                <a href="#" className="text-dark text-decoration-none">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-dark text-decoration-none">
+                  Appointment
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-dark text-decoration-none">
+                  Service
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-dark text-decoration-none">
+                  About Us
+                </a>
+              </li>
+              <li>
+                <a href="#" className="text-dark text-decoration-none">
+                  Contact Us
+                </a>
+              </li>
+            </ul>
+          </Col>
+
+          {/* Hours */}
+          <Col md={4} className="mb-3">
+            <h5 className="fw-bold">Hours</h5>
+            <div className="d-flex">
+              <ul className="list-unstyled col-md-2">
+                <li>Monday:</li>
+                <li>Tuesday:</li>
+                <li>Wednesday:</li>
+                <li>Thursday:</li>
+                <li>Friday:</li>
+              </ul>
+              <ul className="list-unstyled g-col-md-2 ms-5">
+                <li>9:00 - 18:00</li>
+                <li>9:00 - 18:00</li>
+                <li>9:00 - 18:00</li>
+                <li>9:00 - 18:00</li>
+                <li>9:00 - 18:00</li>
+              </ul>
+            </div>
+          </Col>
+
+          {/* Contact Info */}
+          <Col md={4} className="mb-3">
+            <h5 className="fw-bold">Contact</h5>
+            <ul className="list-unstyled">
+              <li>
+                <FontAwesomeIcon icon={faPhone} /> +381 38 500 600
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faEnvelope} />{" "}
+                <a
+                  href="mailto:drejtoria.shskuk@rks-gov.net"
+                  className="text-dark text-decoration-none"
+                >
+                  drejtoria.shskuk@rks-gov.net
+                </a>
+              </li>
+              <li>
+                <FontAwesomeIcon icon={faMapMarkerAlt} /> Lagjja Spitalit,
+                Prishtine 1000, Kosove
+              </li>
+            </ul>
+            <button className="mt-2 btn-reg">Gjej rrugen</button>
+          </Col>
+        </Row>
+
+        {/* Footer Bottom */}
+        <Row className="mt-3">
+          <Col className="text-center">
+            <p className="mb-0 fw-bold">&copy; Qendra Klinike Universitare e Kosoves</p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
