bbc859f28cdb28a80c38a6f2f83bb70924b177aa
Jubair Ibn Khaled <160339037+Juba1r@users.noreply.github.com>
2024-10-16 21:06:51 +0600

    doctor's profile

--- @latest/src/components/UserSection/Doctor/DoctorsProfile/doctorsProfile.jsx
+++ @latest/src/components/UserSection/Doctor/DoctorsProfile/doctorsProfile.jsx
@@ -5,16 +5,16 @@ const doctorsProfile = () => {
   return (
     <div className="container-fluid">
       <div className="doc-bg p-0 mt-5 row">
-        <div className="col ms-5">
+        <div className="col d-flex justify-content-center">
           <img
             src={doctorsimg}
             alt="Doctor's image"
             width="auto"
             height="600px"
-            className="ms-4 mt-5"
+            className=" mt-5"
           />
         </div>
-        <div className="col doctor-title">
+        <div className="col doctor-profile-title">
           <p>
             Hello, I&apos;m
             <p className="fw-bold">Dr.MARLIYN</p>
@@ -24,10 +24,10 @@ const doctorsProfile = () => {
         </div>
       </div>
       <div className="container mt-5">
-        <div className="doctor-details">
+        <div className="doctor-profile-details">
           <h2 className="d-flex justify-content-center">Doctor Details</h2>
 
-          <p className="d-flex justify-content-center mt-5 doctor-details-p">
+          <p className="d-flex justify-content-center mt-5 doctor-profile-details-p">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta,
             distinctio! Lorem ipsum dolor sit amet consectetur adipisicing elit.
             Fugit error, sequi aliquid fuga corrupti quod sapiente quo minus
@@ -46,11 +46,29 @@ const doctorsProfile = () => {
           </p>
         </div>
       </div>
-      <div className="container row">
-        <div className="col d-flex justify-content-center">
-          <input type="text" placeholder="Give a feedback" className="border-2 p-4" />
+      <div className="container col-12 mt-5 d-flex">
+        <div className="col-6 d-flex justify-content-start flex-column gap-3 align-items-center">
+          <input
+            type="text"
+            placeholder="Give a feedback"
+            className="border-1 p-3 w-75 rounded-3"
+          />
+
+          <button className="feedback-send-btn d-flex justify-content-center">Send</button>
         </div>
-        <div className="col"></div>
+        <div className="col-6 d-flex justify-content-center flex-column fw-medium">
+          <h3>Latest Feedbacks</h3>
+          <div className="h-100 p-3 w-100 border border-3 rounded-3 mt-4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, aut! Sint officiis quam nobis cumque quidem fugiat eius in similique voluptate temporibus veniam optio dolor laudantium, esse libero. Exercitationem commodi consectetur adipisci, ea optio veritatis necessitatibus impedit enim et incidunt ?</p>
+          </div>
+          <div className="h-100 p-3 w-100 border border-3 rounded-3 mt-4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, aut! Sint officiis quam nobis cumque quidem fugiat eius in similique voluptate temporibus veniam optio dolor laudantium, esse libero. Exercitationem </p>
+          </div>
+          <div className="h-100 p-3 w-100 border border-3 rounded-3 mt-4">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad, aut! Sint officiis quam nobis cumque quidem fugiat eius in similique voluptate temporibus veniam optio dolor laudantium, esse libero. Exercitationem commodi consectetur adipisci, ea optio veritatis necessitatibus impedit enim et incidunt Lorem ipsum dolor, sit amet consectetur adipisicing elit. Hic, quam!</p>
+          </div>
+          
+        </div>
       </div>
     </div>
   );
